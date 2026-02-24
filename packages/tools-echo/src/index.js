d765737aee7301914e3b0588541fec064446c282
Bill234 <williammpearson@gmail.com>
2026-02-24 18:52:21 +0000

    Add @ng/tools-echo src/index.js

---
+++ packages/tools-echo/src/index.js
@@ -0,0 +1,6 @@
+/**
+ * @ng/tools-echo - Simple echo handle for testing remote package loading
+ */
+
+export { EchoDataSource } from './EchoDataSource.js';
+export { EchoHandle } from './EchoHandle.js';
